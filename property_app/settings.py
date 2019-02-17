9af430fc9c8a6ba13d18204cfa68416219c2a40d
russell13192 <murrayge@mnstate.edu>
2019-02-17 01:14:05 -0800

    MVP completed

--- property_app/settings.py
+++ property_app/settings.py
@@ -142,3 +142,10 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger'
     
 }
+
+# Email Config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='russell13192@gmail.com'
+EMAIL_HOST_PASSWORD='sanyu13192'
+EMAIL_USE_TLS=True
